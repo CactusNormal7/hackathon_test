26ecd5acc8b9ffc2c30965e3fbe260fb2bc057eb
CactusNormal7 <jules.besson7@gmail.com>
2023-10-03 14:37:08 +0200

    d

--- src/components/lobby/lobby.js
+++ src/components/lobby/lobby.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import io from 'socket.io-client'
+import { useEffect } from 'react';
 
 const socketio = io.connect('http://localhost:3001');
 
@@ -8,17 +9,24 @@ const Lobby = () => {
     const [inputValue, setInputValue] = useState("")
 
     const sendMessage = () => {
-        socketio.emit("send_message", {message : inputValue })
+        socketio.emit("send_message", { message: inputValue })
     }
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
-      };
+    };
+
+    useEffect(() => {
+        socketio.on("receive_message", (data) => {
+            alert(data.message)
+        })
+    }, [socketio])
 
     return (
         <div>
             <input placeholder='salut' onChange={handleInputChange} value={inputValue}></input>
             <button onClick={sendMessage}>send</button>
+            <h1>{inputValue}</h1>
         </div>
     );
 }
