1cc6d5a08cdc441a1ba9b73ddf3ef67bbef4a868
CactusNormal7 <jules.besson7@gmail.com>
2023-10-04 10:02:15 +0200

    d

--- src/components/lobby/lobby.js
+++ src/components/lobby/lobby.js
@@ -2,10 +2,24 @@ import { useState } from 'react';
 import io from 'socket.io-client'
 import { useEffect } from 'react';
 
-const socketio = io.connect('https://testsocket-4vkm.onrender.com');
+//        __                            
+//  ____ |__| ____   ____   _____  ____ 
+// /    \|  |/ ___\ / ___\_/ __ \_  __ \
+// |   |  \  / /_/  > /_/  >  ___/|  | \/
+// |___|  /__\___  /\___  / \___  >__|   
+//     \/  /_____//_____/       \/      
+ 
+
+// const socketio  = io.connect('http://localhost:3001');
+const socketio  = io.connect('https://testsocket-4vkm.onrender.com/');
 
 const Lobby = () => {
 
+
+    const room = window.location.pathname.substring(1);
+    socketio.emit("room_number", {room : room})
+
+    const [allMsgTable, setAllMsgTable] = useState({})
     const [inputValue, setInputValue] = useState("")
 
     const sendMessage = () => {
@@ -18,15 +32,27 @@ const Lobby = () => {
 
     useEffect(() => {
         socketio.on("receive_message", (data) => {
-            alert(data.message)
+            let updatedvalue = { [data.sender]: data.data.message };
+            setAllMsgTable(allMsgTable => ({ ...allMsgTable, ...updatedvalue }))
         })
-    }, [socketio])
+        console.log(allMsgTable);
+
+    }, [socketio, allMsgTable])
 
     return (
         <div>
             <input placeholder='salut' onChange={handleInputChange} value={inputValue}></input>
             <button onClick={sendMessage}>send</button>
-            <h1>{inputValue}</h1>
+
+            {Object.keys(allMsgTable).map((key, i) => {
+                return (
+                    <div key={i}>
+                        <h2>{key} : {allMsgTable[key]}</h2>
+                        <hr />
+                    </div>
+                );
+            })}
+
         </div>
     );
 }
